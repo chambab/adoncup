2248aec6b12c7be33ab7bbf9af1a80b7658ed12f
chambab <chambab@gmail.com>
2018-01-14 16:20:14 +0900

    implement app/advertisement update,delete function

--- README.txt
+++ README.txt
@@ -1,1 +1,7 @@
 # description
+
+
+
+# References
+this explains that how to create/retrieve data from mongoose
+https://velopert.com/594
--- apps/advertise.js
+++ apps/advertise.js
@@ -6,7 +6,7 @@ exports.listAdvertises = function(req, res) {
 
 // return an advertise searched by id
 exports.getAdvertise = function(req, res) {
-	Advertise.find({ad_id: req.body.ad_id}, function(err, ad) {
+	Advertise.find({ad_id: req.params.ad_id}, function(err, ad) {
 		if(err) {
 			console.log(err);
 			return res.status(500).json({error:err});
@@ -36,9 +36,32 @@ exports.registerAd = function(req, res) {
 	ad.sub_txt = req.body.sub_txt;
 	ad.save();
 };
-// TODO
+
+// @chambab
+// update advertisement
 exports.updateAd = function(req, res) {
+	Advertise.findById(req.params.ad_id, function(err, ad) {
+		if(err) return res.status(500).json({error: 'database failure'});
+		if(!ad) return res.status(404).json({error: 'Advertise not found'});
+
+		if(req.body.ader) ad.ader = req.body.ader;
+		if(req.body.type) ad.type = req.body.type;
+		if(req.body.main_txt) ad.main_txt = req.body.main_txt;
+		if(req.body.sub_txt) ad.sub_txt = req.body.sub_txt;
+
+		ad.save(Function(err) {
+			if(err) res.status(500).json({error: 'failed to update ad'});
+			res.json({message: 'book updated'});
+		});
+
+	});
 };
 
+// @chambab
+// delete advertisement using ad_id
 exports.deleteAd = function(req, res) {
+	Advertise.remove({_id: req.params.ad_id}, function(err, output)) {
+		if(err) return res.status(500).json({error: 'failed to delete ad'});
+		res.status(204).end();
+	});
 };
